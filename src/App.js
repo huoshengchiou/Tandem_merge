9d0ccf714016a3ac44de17f6ff32aec3d1ba39d6
fonghs <fonghs95@gmail.com>
2020-03-29 11:31:48 +0800

    新增貼文收藏功能

--- src/App.js
+++ src/App.js
@@ -76,7 +76,6 @@ function App() {
               <Sales />
             </Route>
             {/* //community */}
-
             <Route path={`/addpost`}>
               <AddPost />
             </Route>
